ff72c8c02737475a9afec89b43b278f23d7feb4e
asadsanto10 <asad.santo.20@gmail.com>
2023-09-01 22:00:54 +0600

    ts config noemmit remove

--- src/app/middlewares/globalError/globalErrorHandler.middleware.ts
+++ src/app/middlewares/globalError/globalErrorHandler.middleware.ts
@@ -2,8 +2,8 @@ import { ErrorRequestHandler } from 'express';
 import { ZodError } from 'zod';
 import variable from '../../../config';
 import ApiError from '../../../errors/apiError';
-import { castErrorHandler } from '../../../errors/castErrorHandler';
-import validationErrorHandler from '../../../errors/validationErrorHandler';
+// import { castErrorHandler } from '../../../errors/castErrorHandler';
+// import validationErrorHandler from '../../../errors/validationErrorHandler';
 import zodErrorHandler from '../../../errors/zodErrorHandler';
 import { IGenericErrorMessage } from '../../../interface/error.interface';
 import { errorlogger } from '../../../shared/logger';
@@ -19,21 +19,20 @@ const globalErrorHandler: ErrorRequestHandler = (error, req, res, next) => {
 	let errorMessage: IGenericErrorMessage[] = [];
 
 	if (error?.name === 'ValidationError') {
-		const validationError = validationErrorHandler(error);
-		statusCode = validationError?.statusCode;
-		message = validationError?.message;
-		errorMessage = validationError?.errorMessage;
+		// const validationError = validationErrorHandler(error);
+		// statusCode = validationError?.statusCode;
+		// message = validationError?.message;
+		// errorMessage = validationError?.errorMessage;
 	} else if (error instanceof ZodError) {
 		const zodError = zodErrorHandler(error);
 		statusCode = zodError?.statusCode;
 		message = zodError?.message;
 		errorMessage = zodError?.errorMessage;
 	} else if (error?.name === 'CastError') {
-		const castError = castErrorHandler(error);
-
-		statusCode = castError?.statusCode;
-		message = castError?.message;
-		errorMessage = castError?.errorMessage;
+		// const castError = castErrorHandler(error);
+		// statusCode = castError?.statusCode;
+		// message = castError?.message;
+		// errorMessage = castError?.errorMessage;
 	} else if (error instanceof ApiError) {
 		statusCode = error?.statusCode;
 		message = error.message;
